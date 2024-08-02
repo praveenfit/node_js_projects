17461034b69d2d547afba8490f8c7d94fe82ed70
praveenfit <praveengurram1418@gmail.com>
2024-08-03 00:41:40 +0530

    routes setup

--- crudApp.js
+++ crudApp.js
@@ -1,12 +1,16 @@
 const express=require('express')
 const body=require('body-parser')
 const {people}=require('./data')
+const productRoute=require('./productsRouters')
 const app=express()
 const port=process.env.PORT || 3000
 
 //parse from data
 // app.use(express.urlencoded({extended:false}))
+app.use(express.json())
 app.use(body.json())
+// app.use(body.urlencoded({extended:false}))
+app.use('/api',productRoute)
 
 app.get('/getPeople',(req,res)=>{
     res.json(people)
@@ -19,6 +23,28 @@ app.post('/login',(req,res)=>{
         res.json({message:'login success',data:[...people,username]})
     }
     
+})
+
+//////crud operatons///
+app.post('/create',(req,res)=>{
+    const name=req.body
+    people.push(name)
+    if(name){
+        res.json({message:'create success',data:name})
+    }
+
+})
+app.put('/update',(req,res)=>{
+    const id=req.body
+    people.find((items)=>{
+        if(Object.keys(items)==1){
+            items.id=id
+            res.json({message:'update success',data:items})
+            
+        }
+    })
+    
+    console.log(people)
 })
 
 app.listen(port,()=>{
